18a260735562f25b9629b7fff18d253bf976a2e2
Rida Maham <144364768+RidaKhan09@users.noreply.github.com>
2025-08-04 13:14:20 +0500

    Update apis.js

--- Recipe_web/src/utils/apis.js
+++ Recipe_web/src/utils/apis.js
@@ -1,6 +1,6 @@
 
 export const apis = () => {
-    const local = 'http://localhost:5050/';
+    const local = 'https://perfectrecipeapp-2.onrender.com';
   
     const list = {
       registerUser: `${local}user/register`,  

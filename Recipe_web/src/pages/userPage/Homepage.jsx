41f3c07a9593388c38dd42154fe6b20d15524e92
Rida Maham <krida2686@gmail.com>
2025-08-05 17:45:23 +0500

    Your commit message

--- Recipe_web/src/pages/userPage/Homepage.jsx
+++ Recipe_web/src/pages/userPage/Homepage.jsx
@@ -1,7 +1,7 @@
 
 import Hero from '../../components/user/userHome/Hero'
 import ShareRecipe from '../../components/user/userHome/ShareRecipe'
-import TrendingRecipe from '../../components/user/UserHome/TrendingRecipe'
+import TrendingRecipe from '../../components/user/userHome/TrendingRecipe'
 import Blog from '../../components/user/userHome/Blog'
 import Explore_Recipe from '../../components/user/userHome/Explore_Recipe'
 import Newsletter from '../../components/user/userHome/Newsletter'
